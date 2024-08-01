67f732b9e4253c0378149976626492cd2f3cebee
acsa <acsasantos@discente.ufg.br>
2024-07-31 23:58:20 -0300

    lista_produtos e lista_categorias a arrumar

--- app_loja/urls.py
+++ app_loja/urls.py
@@ -4,10 +4,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.lista_produtos, name = 'lista_produtos'),   # localhost:8000
+    path('', views.lista_categorias, name = 'categorias'),   # localhost:8000
     path('produto/<int:id>/', views.detalhe_produto, name = 'detalhe_produto'),   # localhost:800/produto/id
     path('adicionar_ao_carrinho/<int:id>/', views.adicionar_ao_carrinho, name = 'adicionar_ao_carrinho'),   # localhost:800/adicionar_ao_carrinho/id
     path('carrinho/', views.ver_carrinho, name = 'ver_carrinho'),   # localhost:800/carrinho
-    path('produtos/', views.carregar_produtos_csv, name='carregar_produtos_csv'), #localhost:8000/produtos
+    path('produtos/', views.lista_produtos, name='lista_produtos'), #localhost:8000/produtos
 
 ]
--- app_loja/views.py
+++ app_loja/views.py
@@ -5,10 +5,32 @@ import csv
 import os
 from django.conf import settings
 
+def lista_categorias(request):
+    categorias = [
+        "Acessórios e Artigos",
+        "Moda e Acessórios Pessoais",
+        "Bebês e Crianças",
+        "Alimentos e Bebidas",
+        "Casa e Cozinha",
+        "Ferramentas e Equipamentos",
+        "Esporte e Lazer",
+        "Eletrônicos e Tecnologia",
+        "Livros e Mídia",
+        "Cuidados Pessoais e Saúde",
+        "Pet Shop",
+        "Jogos e Consoles"
+    ]
+    
+    context = {
+        'categorias': categorias
+    }
+    
+    return render(request, 'lista_categorias.html', context)
+
 
 def lista_produtos(request):
     produtos = models.Produto.objects.all()
-    return render(request, 'app_loja/lista_app_loja/produtos.html', {'produtos': produtos})
+    return render(request, 'app_loja/produtos.html', {'produtos': produtos})
 
 def detalhe_produto(request, id):
     produto = get_object_or_404( models.Produto, id = id)
@@ -36,42 +58,42 @@ def ver_carrinho(request):
 
 
 
-def carregar_produtos_csv(request):
-    # Caminho para o arquivo CSV
-    csv_file_path = os.path.join(settings.BASE_DIR, '/home/aluno/Área de Trabalho/acsa/Atlantic_Shop/static/', 'dataset-teste.csv')
+# def carregar_produtos_csv(request):
+#     # Caminho para o arquivo CSV
+#     csv_file_path = os.path.join(settings.BASE_DIR, 'static/', 'dataset-teste.csv')
 
-    if request.method == 'POST':
-        try:
-            # Lemos o conteúdo do CSV
-            with open(csv_file_path, newline='', encoding='utf-8') as csvfile:
-                linhas = csv.reader(csvfile)
+#     if request.method == 'POST':
+#         try:
+            # # Lemos o conteúdo do CSV
+            # with open(csv_file_path, newline='', encoding='utf-8') as csvfile:
+            #     linhas = csv.reader(csvfile)
                 
-                # Ignora o cabeçalho
-                next(linhas)
+            #     # Ignora o cabeçalho
+            #     next(linhas)
 
-                produtos_criados = []
-                for linha in linhas:
-                    # Extraímos os dados de cada linha
-                    title, imgUrl, stars, reviews, price, categoryName = linha
+            #     produtos_criados = []
+            #     for linha in linhas:
+            #         # Extraímos os dados de cada linha
+            #         title, imgUrl, stars, reviews, price, categoryName = linha
 
-                    # Criamos um novo produto
-                    produto = models.Produto.objects.create(
-                        nome=title,  # Aqui usamos title para nome
-                        imgUrl=imgUrl,
-                        stars=float(stars),  # Convertendo estrelas para float, se necessário
-                        reviews=int(reviews),  # Convertendo reviews para int, se necessário
-                        preco=float(price),  # Convertendo price para float
-                        categoria=categoryName  # Usando categoryName para categoria
-                    )
-                    produtos_criados.append(produto.nome)  # Adiciona o nome do produto à lista
+            #         # Criamos um novo produto
+            #         produto = models.Produto.objects.create(
+            #             nome=title,  # Aqui usamos title para nome
+            #             imgUrl=imgUrl,
+            #             stars=float(stars),  # Convertendo estrelas para float, se necessário
+            #             reviews=int(reviews),  # Convertendo reviews para int, se necessário
+            #             preco=float(price),  # Convertendo price para float
+            #             categoria=categoryName  # Usando categoryName para categoria
+            #         )
+            #         produtos_criados.append(produto.nome)  # Adiciona o nome do produto à lista
 
             # Obtemos todos os produtos para exibir na tela
-            produtos = models.Produto.objects.all()
-            return render(request, 'app_loja/produtos.html', {'produtos': produtos, 'produtos_criados': produtos_criados})
+    #         produtos = models.Produto.objects.all()
+    #         return render(request, 'app_loja/produtos.html', {'produtos': produtos, 'produtos_criados': produtos_criados})
 
-        except FileNotFoundError:
-            return render(request, 'app_loja/produtos.html', {'error': 'Arquivo CSV não encontrado.'})
-        except Exception as e:
-            return render(request, 'app_loja/produtos.html', {'error': str(e)})
+    #     except FileNotFoundError:
+    #         return render(request, 'app_loja/produtos.html', {'error': 'Arquivo CSV não encontrado.'})
+    #     except Exception as e:
+    #         return render(request, 'app_loja/produtos.html', {'error': str(e)})
     
-    return render(request, 'app_loja/produtos.html', {'error': 'Método não permitido.'})
+    # return render(request, 'app_loja/produtos.html', {'error': 'Método não permitido.'})
